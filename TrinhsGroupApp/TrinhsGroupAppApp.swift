74fb820f1836fbea6137cb301085900ad324894d
Long Nguyen <longnh264@gmail.com>
2022-06-21 10:54:15 +0700

    Init

---
+++ TrinhsGroupApp/TrinhsGroupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrinhsGroupAppApp.swift
+//  TrinhsGroupApp
+//
+//  Created by long on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrinhsGroupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
